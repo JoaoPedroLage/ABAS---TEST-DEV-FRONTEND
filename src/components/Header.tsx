3327527dfefd0b194f401038804b9119629dd9a6
JoaoPedroLage <joaopedro.lage@outlook.com>
2023-04-06 21:51:39 -0300

    feat: create header component, add mui button and a link to list-screen route

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import Link from 'next/link';
+
+export default function LabelBottomNavigation() {
+
+  return (
+    <Link href="/list-screen">
+      <BottomNavigation
+        sx={{ width: 500, height: 200 }}
+        showLabels={true}
+      >
+        <BottomNavigationAction
+          label="Cadastrar uma nova conta bancária"
+          icon={<AccountBalanceIcon />}
+        />
+      </BottomNavigation>
+    </Link>
+  );
+}
