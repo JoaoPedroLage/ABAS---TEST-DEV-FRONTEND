3c5c2ec761cdbe163a4e162f2d2022aa36e14d48
JoaoPedroLage <joaopedro.lage@outlook.com>
2023-04-07 22:13:52 -0300

    feat: add component AccountList to main route/page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import styles from '../styles/page.module.css';
-import Header from '../components/Header';
+import Header from '../components/HeaderMainPage';
+import AccountsList from '@/components/AccountsList';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <Header />
+        <AccountsList />
       </div>
     </main>
   )
