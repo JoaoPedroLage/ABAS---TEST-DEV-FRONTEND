e0ebbc3dc98fd44712c7ffe9c2f3f04de6ff02ee
JoaoPedroLage <joaopedro.lage@outlook.com>
2023-04-10 00:31:18 -0300

    feat: add the CreatedAccountScreen component to SucessRegistration route

---
+++ src/pages/sucess-registration.tsx
@@ -0,0 +1,8 @@
+import CreatedAccountScreen from '@/components/CreatedAccountScreen'
+import React from 'react'
+
+export default function RegistrationScreen() {
+  return (
+    <CreatedAccountScreen/>
+  )
+}
