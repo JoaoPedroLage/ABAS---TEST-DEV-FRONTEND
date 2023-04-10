bdf7d691203073ed2b490708686b947def42df9d
JoaoPedroLage <joaopedro.lage@outlook.com>
2023-04-10 00:26:01 -0300

    feat: add the list of registred accounts with infinite scroll component and making cards one by one with UserBankDataCard component

--- src/components/AccountsList.tsx
+++ src/components/AccountsList.tsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useContext } from 'react';
 import AppContext from '../context/AppContext';
+import InfiniteScroll from 'react-infinite-scroller';
+import UserBankDataCard from './UserBankDataCard';
 
 export default function AccountsList(): JSX.Element {
   const {
     userHasAccounts,
-    setUserHasAccounts
+    setUserHasAccounts,
+    userAccounts,
+    setUserAccounts,
+    numberOfScroll,
+    setNumberOfScroll
   } = useContext(AppContext);
 
   function getAccountsList(): void {
@@ -14,7 +20,15 @@ export default function AccountsList(): JSX.Element {
       setUserHasAccounts(false);
     } else {
       setUserHasAccounts(true);
+      setUserAccounts(JSON.parse(userStorage));
     }
+  }
+
+  function updateScrollBanksList() {
+    const newScroll = userAccounts.slice(0, 10 * numberOfScroll);
+
+    setUserAccounts(newScroll);
+    setNumberOfScroll(numberOfScroll + 1);
   }
 
   useEffect(() => {
@@ -24,10 +38,26 @@ export default function AccountsList(): JSX.Element {
   return (
     <>
       {
-        userHasAccounts ?
-          <span>Usuário tem contas cadastradas</span>
-          :
+        !userHasAccounts ?
           <span>Usuário não tem contas cadastradas</span>
+          :
+          <InfiniteScroll
+            className='Cards'
+            loadMore={updateScrollBanksList}
+            hasMore={true}
+            threshold={50}
+            initialLoad={false}
+            style={{ width: '100%' }}
+          >
+            {
+              userAccounts.map((bank: any, index: number) => (
+                <UserBankDataCard
+                  key={index}
+                  bank={bank}
+                />
+              ))
+            }
+          </InfiniteScroll >
       }
     </>
   );
