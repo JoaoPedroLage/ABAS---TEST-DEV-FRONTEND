86cf7313d449c3724badd50ad2a4031f1e0928e9
JoaoPedroLage <joaopedro.lage@outlook.com>
2023-04-10 23:22:45 -0300

    styles: add styles to the application in all routes to improve the visual appearance and user experience

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import '@/styles/globals.css';
+import '@/styles/mainScreen.css';
 import '@/styles/listScreen.css';
+import '@/styles/registrationScreen.css';
+import '@/styles/accountCreated.css';
 import type { AppProps } from 'next/app';
 import AppProvider from '@/context/AppProvider';
 
