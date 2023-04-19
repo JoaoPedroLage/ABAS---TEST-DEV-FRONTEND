e98cd452cbf34d371fca6dce3971e18e81e6bbc3
JoaoPedroLage <joaopedro.lage@outlook.com>
2023-04-19 17:48:52 -0300

    feat: update text of Registrado card for a better semantic

--- src/components/RegistradoInfo.tsx
+++ src/components/RegistradoInfo.tsx
@@ -4,9 +4,9 @@ export default function RegistradoInfo() {
   return (
     <>
       <p className='registrado-info'>
-        Consulte suas contas bancárias pelo
+        Consulte todas as contas bancárias no seu nome pelo
         <a href='https://registrato.bcb.gov.br/' target='_blank' className='inline'>
-        &nbsp;REGISTRADO
+        &nbsp;REGISTRATO
         </a>, site do Banco do Brasil.
         Basta acessar pelo seu login do gov.br, ir no card "Contas e Relacionamentos", em seguida clicar no botão consultar, aceitar os termos e por fim "Gerar relatório".
       </p>
